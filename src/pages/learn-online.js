4cfa5dae7f1cc7fff5cc826e386d486a5fc4d0fb
Charlotte Fereday <cfereday@thoughtworks.com>
2018-12-31 15:10:40 +0000

    Charlotte - Adds two new pages linked to from home
    - Learn In Person
    - Learn Online

--- src/pages/learn-online.js
+++ src/pages/learn-online.js
@@ -4,12 +4,12 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const LearnInPerson = () => (
+const LearnOnline = () => (
   <Layout>
-    <SEO title="Learn In Person" />
-    <h1>Choose a location we have information on</h1>
+    <SEO title="Learn Online" />
+    <h1>Choose what you're interested in learning below</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default LearnInPerson
+export default LearnOnline
